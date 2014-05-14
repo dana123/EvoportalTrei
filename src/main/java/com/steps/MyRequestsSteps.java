6fa2bb8725dbcaa9c6497f2616d7ffda3afd14e2
alexandruduminciuc <alexandruduminciuc@EVO022.localdomain>
2014-05-14 11:34:58 +0300

    StepGroup MyRequests

--- src/main/java/com/steps/MyRequestsSteps.java
+++ src/main/java/com/steps/MyRequestsSteps.java
@@ -1,6 +1,8 @@
 package com.steps;
 
 import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.StepGroup;
+import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.steps.ScenarioSteps;
 
 import com.pages.MyRequestsPage;
@@ -8,6 +10,11 @@ import com.pages.MyRequestsPage;
 public class MyRequestsSteps extends ScenarioSteps{
 	
 	MyRequestsPage myRequestPage;
+	
+	@Steps
+	LogInSteps logInSteps;
+	
+	
 	
 	@Step
 	public void clickMyRequestsPage(){
@@ -24,8 +31,37 @@ public class MyRequestsSteps extends ScenarioSteps{
 	}
 	
 	@Step
+	public void checkOneFiveFilter(){
+		myRequestPage.checkOneFiveDaysFilter();
+	}
+	@Step
 	public void applyFiltersOnMyRequests(){
 		myRequestPage.clickApplyFilterButton();
+	}
+	
+	@Step
+	public int verifyIfTableExists(){
+		if(myRequestPage.checkIfRequestsTableExists()==true)		return 1;
+		return 0;
+	}
+	@StepGroup
+	public void filterMyRequests() {
+		logInSteps.openPage();
+		logInSteps.logIn("alexandruduminciuc", "alexandru87");
+		
+		//New Requests
+		
+		clickMyRequestsPage();
+		checkHolidayCheckBox();
+		checkVacationWithoutPayment();
+		checkOneFiveFilter();
+		applyFiltersOnMyRequests();
+		if(verifyIfTableExists()==1){
+			//cauta corectitudinea datelor
+		}
+		
+		//Cancel or withdraw requests
+		
 	}
 
 			
