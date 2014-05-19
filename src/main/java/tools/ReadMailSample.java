442d94651528154630570db20754e7117167afbc
horatiuencian <horatiuencian@EVZPC023.localdomain>
2014-05-19 13:50:25 +0300

    read mail

---
+++ src/main/java/tools/ReadMailSample.java
@@ -0,0 +1,178 @@
+package tools;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.BodyPart;
+import javax.mail.Flags;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.NoSuchProviderException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.Flags.Flag;
+import javax.mail.search.FlagTerm;
+ 
+public class ReadMailSample {
+    Properties properties = null;
+    private Session session = null;
+    private Store store = null;
+    private Folder inbox = null;
+    private String userName = "horatiu.encian@evozon.com";// provide user name
+    private String password = "Jungla123.";// provide password
+ 
+    public ReadMailSample() {
+ 
+    }
+ 
+    public void readMails() {
+        properties = new Properties();
+        properties.setProperty("mail.host", "mail.evozon.com");
+        properties.setProperty("mail.port", "993");
+        properties.setProperty("mail.transport.protocol", "imaps");
+        session = Session.getInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(userName, password);
+                    }
+                });
+        try {
+            store = session.getStore("imaps");
+            store.connect();
+            inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
+            Message messages[] = inbox.search(new FlagTerm(
+                    new Flags(Flag.SEEN), false));
+            ;
+            System.out.println("Number of mails = " + messages.length);
+            for (int i = messages.length-1; i < messages.length; i++) {
+                Message message = messages[i];
+                Address[] from = message.getFrom();
+                System.out.println("-------------------------------");
+                System.out.println("Date : " + message.getSentDate());
+                System.out.println("From : " + from[0]);
+                System.out.println("Subject: " + message.getSubject());
+                System.out.println("Content :");
+                processMessageBody(message);
+                System.out.println("--------------------------------");
+ 
+            }
+            inbox.close(true);
+            store.close();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    
+   
+ 
+    public void processMessageBody(Message message) {
+        try {
+            Object content = message.getContent();
+            // check for string
+            // then check for multipart
+            if (content instanceof String) {
+                System.out.println(content);
+            } else if (content instanceof Multipart) {
+                Multipart multiPart = (Multipart) content;
+                procesMultiPart(multiPart);
+            } else if (content instanceof InputStream) {
+                InputStream inStream = (InputStream) content;
+                int ch;
+                while ((ch = inStream.read()) != -1) {
+                    System.out.write(ch);
+                }
+ 
+            }
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public void procesMultiPart(Multipart content) {
+ 
+        try {
+            int multiPartCount = content.getCount();
+            for (int i = 0; i < multiPartCount; i++) {
+                BodyPart bodyPart = content.getBodyPart(i);
+                Object o;
+ 
+                o = bodyPart.getContent();
+                if (o instanceof String) {
+                    System.out.println(o);
+                } else if (o instanceof Multipart) {
+                    procesMultiPart((Multipart) o);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+  public void readLastMail(){
+	  
+	  String subject;
+	  
+	  properties = new Properties();
+      properties.setProperty("mail.host", "mail.evozon.com");
+      properties.setProperty("mail.port", "993");
+      properties.setProperty("mail.transport.protocol", "imaps");
+      session = Session.getInstance(properties,
+              new javax.mail.Authenticator() {
+                  protected PasswordAuthentication getPasswordAuthentication() {
+                      return new PasswordAuthentication(userName, password);
+                  }
+              });
+      try {
+          store = session.getStore("imaps");
+          store.connect();
+          inbox = store.getFolder("INBOX");
+          inbox.open(Folder.READ_ONLY);
+          Message messages[] = inbox.search(new FlagTerm(
+                  new Flags(Flag.SEEN), false));
+          ;
+          System.out.println("Number of mails = " + messages.length);
+         
+              Message message = messages[messages.length-1];
+              Address[] from = message.getFrom();
+              System.out.println("-------------------------------");
+              System.out.println("Date : " + message.getSentDate());
+              System.out.println("From : " + from[0]);
+              System.out.println("Subject: " + message.getSubject());
+              subject = message.getSubject();
+              System.out.println("Subject is: " + subject);
+              System.out.println("Content :");
+              processMessageBody(message);
+              System.out.println("--------------------------------");
+
+          
+          inbox.close(true);
+          store.close();
+      } catch (NoSuchProviderException e) {
+          e.printStackTrace();
+      } catch (MessagingException e) {
+          e.printStackTrace();
+      }
+  }
+  
+    
+    
+    
+    public static void main(String[] args) {
+        ReadMailSample sample = new ReadMailSample();
+        sample.readLastMail();
+    }
+ 
+}
